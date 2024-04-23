2cf60c6275043d056bb3527c8091bcb364a4b8a7
ohhawjenog <tjdtnr0428@naver.com>
2024-04-23 11:02:19 +0900

    feat 8. 열거형

---
+++ 240419/240419/Program.cs
@@ -0,0 +1,66 @@
+﻿// 6. IF, Switch 조건문
+
+bool isActive = true;
+
+if(isActive)
+{
+    Console.WriteLine("작동 중 입니다.");
+}
+else
+{
+    Console.WriteLine("정지상태 입니다.");
+}
+
+int number = 5;
+
+// 비교 연산자
+if(number == 0)      Console.WriteLine("");
+else if(number != 0) Console.WriteLine("");
+else if(number > 0)  Console.WriteLine("");
+else if(number < 0)  Console.WriteLine("");
+else if(number >= 0) Console.WriteLine("");
+else if(number <= 0) Console.WriteLine("");
+
+// 논리 연산자 AND(&&), OR(||), NOT(!)
+if (number == 0 && isActive == true)       Console.WriteLine("");
+else if (number == 0 || isActive == false) Console.WriteLine("");
+else if (!isActive)                        Console.WriteLine("");
+
+// 번호에 따른 시퀀스 작동
+int status = 0;
+
+switch(status)
+{
+    case 0:
+        Console.WriteLine("0번 상태입니다.");
+        break;
+    case 1:
+        Console.WriteLine("1번 상태입니다.");
+        break;
+    case 2:
+        Console.WriteLine("2번 상태입니다.");
+        break;
+}
+
+// 실습3. PLC 5초 딜레이 신호등을 if문 또는 switch case문으로 작성해 봅니다.
+// 조건: status / 황색(0), 적색(1), 녹색(2)
+// M10(0), M11(1), M12(2) -> Console.WriteLine("황색 전구 ON");
+// Timer -> Console.WriteLine("5초 Timer On");
+
+for(int i = 0; i < 3; i++)
+{
+    if(i == 0)
+    {
+        Console.WriteLine("황색 전구 ON");
+    }
+    else if(i == 1)
+    {
+        Console.WriteLine("적색 전구 ON");
+    }
+    else if(i == 2)
+    {
+        Console.WriteLine("녹색 전구 ON");
+    }
+
+    Console.WriteLine("5초 Timer On");
+}
---
+++ 240422/240422/Program.cs
@@ -0,0 +1,40 @@
+﻿// 8. 열거형
+////
+//열거형 
+using System.Security.Authentication;
+
+public enum Option
+{
+    RegisterBook = 5,
+    SearchBook,
+    RegisterMember,
+    SearReservation,
+    MakeReservation
+}
+
+public class Student
+{   
+
+    public static void Main()
+    {
+        //사용을 위해 선언
+        Option op = Option.RegisterBook;
+
+        switch (op)
+        { 
+            case Option.RegisterBook:
+                break;
+            case Option.SearchBook:
+                break;
+            case Option.RegisterMember:
+                break;
+            case Option.MakeReservation:
+                break;
+        }
+
+        if (op == Option.RegisterMember) 
+        { 
+        
+        }
+    }
+}
